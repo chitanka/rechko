ef5317adbbafabe039cc2692f893d2a354aae602
borislav <b.manolov@gmail.com>
2012-04-09 19:23:33 +0200

    [view] missing end tag for ul

--- apps/frontend/templates/_personal.php
+++ apps/frontend/templates/_personal.php
@@ -1,9 +1,10 @@
 <div id="personal">
-	<?php if ($sf_user->isAuthenticated()): ?>
 	<ul>
+	<?php if ($sf_user->isAuthenticated()): ?>
 		<li class="profile"><?php echo link_to($sf_user->getRealname(), '@user?slug='.$sf_user->getGuardUser()->getSlug()) ?></li>
 		<li class="signout"><?php echo link_to('Изход', '@signout') ?></li>
 	<?php else: ?>
 		<li class="signin"><?php echo link_to('Вход', '@signin') ?></li>
 	<?php endif ?>
+	</ul>
 </div>
