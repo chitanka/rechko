fe7b22543cefd3af10a8bcee5eea6e34bed9acfc
bilyana <bilyana.kostova@provisionsofia.com>
2018-03-17 17:24:27 +0200

    bmValidatorSpam - improvements

--- plugins/bmFeedbackPlugin/lib/validator/bmValidatorSpam.class.php
+++ plugins/bmFeedbackPlugin/lib/validator/bmValidatorSpam.class.php
@@ -1,28 +1,28 @@
 <?php
 class bmValidatorSpam extends sfValidatorBase
 {
-	private $_maxLinkCount = 2;
+    private $_maxLinkCount = 0;
 
 // 	protected function configure($options = array(), $messages = array())
 // 	{
 // 		parent::configure($options, $messages);
 // 	}
 
-	protected function doClean($value)
-	{
-		if (strpos($value, '<a') !== false) {
-			throw new sfValidatorError($this, 'html not allowed: a');
-		}
-		if (strpos($value, '[url') !== false) {
-			throw new sfValidatorError($this, 'bbcode not allowed: url');
-		}
-		if (strpos($value, '[link') !== false) {
-			throw new sfValidatorError($this, 'bbcode not allowed: link');
-		}
-		if (substr_count($value, 'http') > $this->_maxLinkCount) {
-			throw new sfValidatorError($this, "link count exceeds $this->_maxLinkCount");
-		}
+    protected function doClean($value)
+    {
+        if (strpos($value, '<a') !== false) {
+            throw new sfValidatorError($this, 'html not allowed: a');
+        }
+        if (strpos($value, '[url') !== false) {
+            throw new sfValidatorError($this, 'bbcode not allowed: url');
+        }
+        if (strpos($value, '[link') !== false) {
+            throw new sfValidatorError($this, 'bbcode not allowed: link');
+        }
+        if (substr_count($value, 'http') > $this->_maxLinkCount) {
+            throw new sfValidatorError($this, "link count exceeds $this->_maxLinkCount");
+        }
 
-		return $value;
-	}
+        return $value;
+    }
 }
